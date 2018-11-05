e4a874f92f5fca8b3ecffde49c2d1aa3eac8f6ef
j0mbajuice <josh.ventocilla@gmail.com>
2018-11-04 20:13:33 -0800

    testing commit

---
+++ jenkins/Hello172Leias.java
@@ -0,0 +1,9 @@
+
+public class Hello172Leias {
+	
+	public static void main(String[] args) {
+		System.out.println("This is Team Leais.");
+		System.out.println("We are setting up Jenkins for out Git!");
+	}
+
+}
